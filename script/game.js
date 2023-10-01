1b54bd9d40cf4f8ac93feb1fe00c23e931bd6438
Ankit Varshney <lyfflame45@gmail.com>
2023-10-01 15:12:21 +0530

    added game screen

---
+++ script/game.js
@@ -0,0 +1,5 @@
+
+let score = 50;
+document.getElementById("p1").innerHTML = `Score: ${score}`;
+document.getElementById("p1").style.fontSize = "25px";
+
---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+document.getElementById("btn").addEventListener("click", remove,true);
+function remove() {
+    window.location.href="./game_screen.html"
+}
+// let score = 0;
+document.getElementById("score").innerHTML = `Score: ${score}`;
+document.getElementById("score").style.fontSize = "25px";
+
+    let score = 5;
+    document.getElementById("p1").innerHTML = `Score: ${score}`;
+    // document.getElementById("score").style.fontSize = "25px";
+   
+
