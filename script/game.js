fc01d569448007c0465ac67bc3a163ae96f21782
Ankit Varshney <lyfflame45@gmail.com>
2023-10-01 17:49:23 +0530

    added falling characters

--- script/game.js
+++ script/game.js
@@ -1,5 +1,36 @@
 
-let score = 50;
-document.getElementById("p1").innerHTML = `Score: ${score}`;
-document.getElementById("p1").style.fontSize = "25px";
+// let score = 50;
+// document.getElementById("p1").innerHTML = `Score: ${score}`;
+// document.getElementById("p1").style.fontSize = "25px";
+function getRandomChar() {
+   
+    charCode = Math.floor(Math.random() * 26) + 97;
+    // console.log(String.fromCharCode(charCode))
+    console.log(charCode);
+    return String.fromCharCode(charCode);
+}
+function createBubble() {
+    const bubble = document.createElement("div");
+    bubble.classList.add("bubble");
+    bubble.style.left = `${Math.random() * 70}%`; 
+    bubble.textContent = getRandomChar();
+    console.log(getRandomChar());    
+    bubble.style.backgroundColor = getRandomColor();
+    document.querySelector(".container").appendChild(bubble);
 
+    bubble.addEventListener("animationiteration", () => {
+        
+        bubble.remove();
+    });
+}
+
+function getRandomColor() {
+    let maxVal = 0xFFFFFF; 
+    let randomNumber = Math.floor(Math.random() * maxVal).toString(16);
+    let randCol = randomNumber.padStart(6, 0);
+    return `#${randCol.toUpperCase()}`;
+
+    
+}
+
+setInterval(createBubble, 2000);
--- script/script.js
+++ script/script.js
@@ -7,8 +7,6 @@ function remove() {
 document.getElementById("score").innerHTML = `Score: ${score}`;
 document.getElementById("score").style.fontSize = "25px";
 
-    let score = 5;
-    document.getElementById("p1").innerHTML = `Score: ${score}`;
-    // document.getElementById("score").style.fontSize = "25px";
+    
    
 
