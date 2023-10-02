917e9ee8bf01ba2de884648ff59cd616175ddeb0
Ankit Varshney <lyfflame45@gmail.com>
2023-10-02 13:29:01 +0530

    added canvas

--- script/game.js
+++ script/game.js
@@ -1,13 +1,30 @@
-
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
 
 let score = 0;
-
-let repeat = 1000; 
-let minRepeat = 300;
-let intervalId = setInterval(createBubble, repeat);
+const maxbubbles=10
+let speed = 0.5;
+let maxSpeed = 6;
 const generatedChars = new Set();
-document.getElementById("score").innerHTML = `Score:${score}`;
-document.getElementById("score").style.fontSize = "25px ";
+let minRepeat = 300;
+let repeat = 2000;
+let intervalId = setInterval(createBubble, repeat);
+
+
+document.getElementById("score").innerHTML = `Score: ${score}`;
+document.getElementById("score").style.fontSize = "25px";
+document.getElementById("score").style.color = "white";
+
+bubbleMap = new Map();
+
+function getRandomColor() {
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += Math.floor(Math.random() * 10);
+  }
+  return color;
+}
 function getRandomChar() {
     let char;
     do {
@@ -19,74 +36,76 @@ function getRandomChar() {
     return char;
    
 }
- bubbleMap = new Map();
-function createBubble() {
-    const bubble = document.createElement("div");
-    bubble.classList.add("bubble");
-    bubble.style.left = `${Math.random() * 90}%`; 
-    bubbleText = getRandomChar();
-    bubble.textContent = bubbleText;
-    bubble.style.backgroundColor = getRandomColor();
-    document.querySelector(".container").appendChild(bubble);
-    bubbleMap.set(bubbleText, bubble);
-
-
-    bubble.addEventListener("animationiteration", function ()  {
-        
-        this.remove();
-        const text = this.textContent;
-        bubbleMap.delete(text);
-        console.log("this part is running" +text);
-        generatedChars.delete(text);
-    });
-    adjustSpeed();
-}
-document.addEventListener('keypress', handleKeyDown);
-
-
-function getRandomColor() {
-    // let maxVal = 0xFFFFFF; 
-    // let randomNumber = Math.floor(Math.random() * maxVal).toString(16);
-    // let randCol = randomNumber.padStart(6, 0);
-    // return `#${randCol.toUpperCase()}`;
-    var color = '#';
-    for (var i = 0; i < 6; i++){
-        color += Math.floor(Math.random() * 10);
-    }
-    return color;
-    
-}
 function adjustSpeed() {
-    console.log(repeat);
-   
-    if (score % 5== 0 && score!=0) {
-        repeat = Math.max(repeat - 200, minRepeat);
-        clearInterval(intervalId); 
-        intervalId = setInterval(createBubble, repeat); 
+  if (score % 5 == 0 && score != 0) {
+      speed = Math.min(speed+0.05, maxSpeed);
+      repeat = Math.max(repeat - 500, minRepeat);
+      clearInterval(intervalId); 
+      intervalId = setInterval(createBubble, repeat); 
+  }
+}
+
+function createBubble() {
+    if (bubbleMap.size < maxbubbles) {
+        const bubbleText = getRandomChar();
+        const radius = 25;
+        const x = Math.random() * (canvas.width - 2 * radius) + radius;
+        const y = canvas.height + radius;
+
+        bubbleMap.set(bubbleText, { x, y, radius });
+
+        adjustSpeed();
     }
 }
+
+function drawBubbles() {
+  bubbleMap.forEach((bubble, key) => {
+    const { x, y, radius } = bubble;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.fillStyle = getRandomColor()
+          ;
+    ctx.fill();
+    ctx.fillStyle = "#fff"; 
+    ctx.font = "20px Arial"; 
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(key, x, y);
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  bubbleMap.forEach((bubble, key) => {
+    if (bubble.y - bubble.radius > 0) {
+      bubble.y -= speed; 
+    } else {
+      bubbleMap.delete(key);
+      generatedChars.delete(key);
+    }
+  });
+
+  drawBubbles();
+}
+
+document.addEventListener("keypress", handleKeyDown);
 
 function handleKeyDown(event) {
-    const pressedKey = event.key;
-    
-    if (bubbleMap.has(pressedKey)) {
-        const bubble = bubbleMap.get(pressedKey);
-        bubble.remove();
-        bubbleMap.delete(pressedKey);
-        generatedChars.delete(pressedKey);
-        score++;
-        document.getElementById("score").innerHTML = `Score:${score}`;
-        adjustSpeed();
-               
-    }
-    else {
-        score--;
-        document.getElementById("score").innerHTML = `Score:${score}`;
-    }
-   
+  const pressedKey = event.key;
 
+  if (bubbleMap.has(pressedKey)) {
+    bubbleMap.delete(pressedKey);
+    generatedChars.delete(pressedKey);
+    score++;
+    document.getElementById("score").innerHTML = `Score: ${score}`;
+    adjustSpeed();
+  } else {
+    score--;
+    document.getElementById("score").innerHTML = `Score: ${score}`;
+  }
 }
 
-
-console.log(repeat);
-
+animate();
+setInterval(createBubble, repeat);
