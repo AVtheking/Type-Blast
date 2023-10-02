44f1a0482d8e5920a1caed87cb0d8c57891f66fa
Ankit Varshney <lyfflame45@gmail.com>
2023-10-03 02:17:30 +0530

    added explosive animation

---
+++ script/Particle.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+export default class Particle {
+  constructor(x, y, radius, color, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.dx = dx;
+    this.dy = dy;
+    this.alpha = 1;
+  }
+
+  draw() {
+    ctx.save();
+    ctx.globalAlpha = this.alpha;
+    ctx.fillStyle = this.color;
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.restore();
+  }
+
+  update() {
+    this.draw();
+    this.alpha -= 0.02;
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+}
+// export default class Particle {
+//   constructor(x, y, radius, color, dx, dy) {
+//     this.x = x;
+//     this.y = y;
+//     this.radius = radius;
+//     this.color = color;
+//     this.dx = dx;
+//     this.dy = dy;
+//     this.alpha = 1;
+//   }
+//   draw() {
+//     ctx.save();
+//     ctx.globalAlpha = this.alpha;
+//     ctx.fillStyle = this.color;
+//     ctx.beginPath();
+//     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+//     ctx.fill();
+//     ctx.restore();
+//   }
+//   update() {
+//     this.draw();
+//     this.alpha -= 0.02;
+//     this.x += this.dx;
+//     this.y = this.dy;
+//   }
+// }
--- script/game.js
+++ script/game.js
@@ -1,16 +1,17 @@
+import Particle from "./Particle.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-// ctx.imageSmoothingEnabled = false;
+
 let myScore = document.getElementById("score");
 let endScore = document.getElementById("endScore");
-
-var score = 0;
+let score = 0;
 const maxbubbles = 10;
 let speed = 0.5;
 let maxSpeed = 6;
 const generatedChars = new Set();
 let minRepeat = 300;
-let repeat = 1300;
+let repeat = 1500;
 let intervalId = setInterval(createBubble, repeat);
 var hearts = document.getElementsByClassName("hearts");
 let health = 10;
@@ -20,12 +21,24 @@ myScore.innerHTML = `Score: ${score}`;
 myScore.style.fontSize = "25px";
 myScore.style.color = "white";
 myScore.style.marginLeft = "1vw";
-
+let id;
+const particles = [];
 const bubbleMap = new Map();
+
+function triggerExplosion(x, y) {
+  for (let i = 0; i < 30; i++) {
+    const radius = Math.random() * 2 + 1;
+    const color = getRandomColor();
+    const dx = (Math.random() - 0.5) * 3;
+    const dy = (Math.random() - 0.5) * 3;
+
+    particles.push(new Particle(x, y, radius, color, dx, dy));
+  }
+}
 
 function damage() {
   health--;
-  console.log(health);
+  // console.log(health);
   if (health == 8) {
     hearts[0].style.opacity = 0;
   }
@@ -43,12 +56,14 @@ function damage() {
     gameOver();
   }
 }
+
 function getRandomColor() {
   let maxVal = 0xffffff;
   let randomNumber = Math.floor(Math.random() * maxVal).toString(16);
   let randCol = randomNumber.padStart(6, 0);
   return `#${randCol.toUpperCase()}`;
 }
+
 function getRandomChar() {
   let char;
   do {
@@ -58,6 +73,7 @@ function getRandomChar() {
   generatedChars.add(char);
   return char;
 }
+
 function adjustSpeed() {
   if (score % 5 == 0 && score != 0) {
     speed = Math.min(speed + 0.02, maxSpeed);
@@ -96,9 +112,43 @@ function drawBubbles() {
   });
 }
 
+document.addEventListener("keypress", handleKeyDown);
+
+function handleKeyDown(event) {
+  if (health == 0) {
+    return;
+  }
+  const pressedKey = event.key;
+
+  if (bubbleMap.has(pressedKey)) {
+    const bubble = bubbleMap.get(pressedKey);
+    bubbleMap.delete(pressedKey);
+    generatedChars.delete(pressedKey);
+    score++;
+    document.getElementById("score").innerHTML = `Score: ${score}`;
+    adjustSpeed();
+    triggerExplosion(bubble.x, bubble.y);
+  } else {
+    sound.pause();
+
+    sound.play();
+    score = Math.max(score - 1, 0);
+    console.log(score);
+    document.getElementById("score").innerHTML = `Score: ${score}`;
+  }
+}
 function animate() {
-  const id = requestAnimationFrame(animate);
+  id = requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  console.log(speed);
+  console.log(repeat);
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
 
   bubbleMap.forEach((bubble, key) => {
     if (bubble.y - bubble.radius > 0) {
@@ -116,31 +166,6 @@ function animate() {
   }
 
   drawBubbles();
-}
-
-document.addEventListener("keypress", handleKeyDown);
-
-function handleKeyDown(event) {
-  if (health == 0) {
-    return;
-  }
-  const pressedKey = event.key;
-
-  if (bubbleMap.has(pressedKey)) {
-    const bubble = bubbleMap.get(pressedKey);
-    bubbleMap.delete(pressedKey);
-    generatedChars.delete(pressedKey);
-    score++;
-    document.getElementById("score").innerHTML = `Score: ${score}`;
-    adjustSpeed();
-  } else {
-    sound.pause();
-
-    sound.play();
-    score = Math.max(score - 1, 0);
-    console.log(score);
-    document.getElementById("score").innerHTML = `Score: ${score}`;
-  }
 }
 function gameOver() {
   gameOverSound.play();
@@ -165,8 +190,8 @@ function gameOver() {
     hearts[2].style.opacity = 1;
     hearts[3].style.opacity = 1;
     hearts[4].style.opacity = 1;
-    speed = 1;
-    repeat = 2000;
+    speed = 0.5;
+    repeat = 1500;
     clearInterval(intervalId);
     intervalId = setInterval(createBubble, repeat);
     animate();
